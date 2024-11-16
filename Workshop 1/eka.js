f0982bbf0cba35ae074a42fb9c9b8a220268fe5d
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-16 04:10:53 +0200

    d

--- Workshop 1/eka.js
+++ Workshop 1/eka.js
@@ -53,3 +53,53 @@ if (a+b > 100) { alert("Too big!");
 document.write("<h2>Sum Calculation</h2>");
 document.write("<p>" + a + " + " + b + " = <strong>" + (a+b) + "</strong></p>"); }
 console.log(addNumbers(a, b));
+
+var person = {
+firstName:"John",
+lastName:"Doe",
+age:50,
+eyeColor:"blue",
+address:"Ratatie 3",
+telephoneNumber:"0100100", };
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.eyeColor);
+console.log(person.address);
+console.log(person.telephoneNumber);
+
+var person = [
+    {firstName:"John",
+lastName:"Doe",
+age:50,
+eyeColor:"blue",
+address:"Ratatie 3",
+telephoneNumber:"0100100"
+},
+{firstName:"Joe",
+lastName:"Loe",
+age:25,
+eyeColor:"green",
+address:"Ratikkakuja 5",
+telephoneNumber:"0200209"},
+{firstName:"Riley",
+lastName:"Resting",
+age:80,
+eyeColor:"grey",
+address:"Pasilanraitti 3",
+telephoneNumber:"45609223"},
+{ firstName:"Oili",
+lastName:"Matikainen",
+age:15,
+eyeColor:"blue",
+address:"Omenatie 94",
+telephoneNumber:"042394238"}]
+;
+for (var i = 0; i < person.length; i++) {
+console.log(person[i].firstName);
+console.log(person[i].lastName);
+console.log(person[i].age);
+console.log(person[i].eyeColor);
+console.log(person[i].address);
+console.log(person[i].telephoneNumber); }
+
