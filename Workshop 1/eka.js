fa22828bad0645aceb4db09de5fc176cd04a3d39
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-16 04:39:20 +0200

    Update eka.js

--- Workshop 1/eka.js
+++ Workshop 1/eka.js
@@ -2,7 +2,9 @@ document.write("Hello World!");
 alert("Hello World!");
 console.log("Hello World")
 
+
 window.alert(prompt("Anna nimesi..."))
+
 
 var x = 5;
 var y = 6;
@@ -11,12 +13,14 @@ console.log(z);
 alert(z);
 document.write(z);
 
+
 var x = 18;
 var order = "Beer";
 if (x > 50) { document.write("He's over 50!"); }
 else if (x <= 50 && x> 30) { document.write("Middle aged man, who ordered some " + order); }
 else if (x <= 30 && x >= 18) { document.write("Young adult, who ordered some " + order); }
 else { document.write("It seems you're bit underaged.") }
+
 
 var x = 30;
 var order = "Beer";
@@ -31,17 +35,20 @@ console.log("Young adult, who ordered some " + order); }
 else { document.write("It seems you're bit underaged.")
 console.log("It seems you're bit underaged.") }
 
+
 var arr = [];
 for (var i = 0; i < 15; i++)
 { arr.push(Math.floor(Math.random() * 100)); } 
 console.log(arr);
 document.write(arr);
 
+
 var arr = [];
 for (var i = 0; i < 15; i++)
 { arr.push(Math.random() * 100); } 
 console.log(arr);
 document.write(arr);
+
 
 function addNumbers(a, b) {
 return a + b;}
@@ -53,6 +60,7 @@ if (a+b > 100) { alert("Too big!");
 document.write("<h2>Sum Calculation</h2>");
 document.write("<p>" + a + " + " + b + " = <strong>" + (a+b) + "</strong></p>"); }
 console.log(addNumbers(a, b));
+
 
 var person = {
 firstName:"John",
@@ -68,33 +76,36 @@ console.log(person.eyeColor);
 console.log(person.address);
 console.log(person.telephoneNumber);
 
+
 var person = [
     {firstName:"John",
 lastName:"Doe",
 age:50,
 eyeColor:"blue",
 address:"Ratatie 3",
-telephoneNumber:"0100100"
-},
+telephoneNumber:"0100100"},
+
 {firstName:"Joe",
 lastName:"Loe",
 age:25,
 eyeColor:"green",
 address:"Ratikkakuja 5",
 telephoneNumber:"0200209"},
+
 {firstName:"Riley",
 lastName:"Resting",
 age:80,
 eyeColor:"grey",
 address:"Pasilanraitti 3",
 telephoneNumber:"45609223"},
+
 { firstName:"Oili",
 lastName:"Matikainen",
 age:15,
 eyeColor:"blue",
 address:"Omenatie 94",
-telephoneNumber:"042394238"}]
-;
+telephoneNumber:"042394238"}];
+
 for (var i = 0; i < person.length; i++) {
 console.log(person[i].firstName);
 console.log(person[i].lastName);
@@ -103,3 +114,19 @@ console.log(person[i].eyeColor);
 console.log(person[i].address);
 console.log(person[i].telephoneNumber); }
 
+
+var i = 5; 
+var j = 10;
+var k = 25; 
+var l = "Joe";
+
+if (i < j) {
+console.log(i); } 
+else if (j > k) {
+console.log(k);}
+else {console.log(l)};
+
+
+var arr = [3, 5, 7];
+
+for (var i = 0; i < arr.length; ++i) { console.log(arr[i]); }
