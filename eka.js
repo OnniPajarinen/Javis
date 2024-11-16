2547b921accc95e861c68cc9587e3b4570885389
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-16 02:12:48 +0200

    Update eka.js

---
+++ eka.js
@@ -0,0 +1,12 @@
+document.write("Hello World!");
+alert("Hello World!");
+console.log("Hello World")
+
+window.alert(prompt("Anna nimesi..."))
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+alert(z);
+document.write(z);
