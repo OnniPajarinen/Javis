16bf5ed2992b7a69bc78e0b9fb24bd96b4ec6e85
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-22 20:58:10 +0200

    d

--- Workshop 2/Script/scripti.js
+++ Workshop 2/Script/scripti.js
@@ -1,1 +1,10 @@
-document.write("Toimiiko tämä?")
+if (navigator.userAgent.includes("Mozilla")) {
+    window.location.href = "https://www.mozilla.org";
+}
+
+else {window.location.href = "https://www.gigantti.fi"}
+
+for (let i = 0; i < 50; i++) {
+
+document.write('<strong><i>"If I had nine hours to chop down a tree, I\'d spend the first six sharpening my ax."</i></strong><br>')
+document.write("<i>-- Abraham Lincoln</i><br>")}
