959bb079b5ca9d7d3ba24630a3f488373008a6e5
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-22 21:37:22 +0200

    Update sivutin.js

--- Workshop 2/Script/sivutin.js
+++ Workshop 2/Script/sivutin.js
@@ -1,3 +1,10 @@
 var Student = confirm("Are you a student?")
-if (Student) {document.write("Welcome student!")}
-else {document.write("Sorry, this page is for students only!")}
+if (Student) {document.write("Welcome student!<br><br>")}
+else {document.write("Sorry, this page is for students only!<br><br>")}
+
+var images =[
+"http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
+"http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
+"http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg"
+];
+document.write('<img src=http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg">')
