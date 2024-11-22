40c125bdb1551e86c6953a76852dbeabcf2e89fa
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-22 21:54:36 +0200

    k

--- Workshop 2/Script/sivutin.js
+++ Workshop 2/Script/sivutin.js
@@ -7,4 +7,5 @@ var images =[
 "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
 "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg"
 ];
-document.write('<img src=http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg">')
+var chosen = Math.floor(Math.random() * 3);
+document.write('<img src="' + images[chosen] + '"><br><br>')
