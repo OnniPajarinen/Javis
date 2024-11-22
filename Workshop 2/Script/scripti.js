8888acfb6cb1642bc2b7046196dbd0b40a9cf566
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-22 21:27:23 +0200

    s

--- Workshop 2/Script/scripti.js
+++ Workshop 2/Script/scripti.js
@@ -1,10 +1,10 @@
+for (let i = 0; i < 50; i++) {
+
+document.write('<strong><i>"If I had nine hours to chop down a tree, I\'d spend the first six sharpening my ax."</i></strong><br>')
+document.write("<i>-- Abraham Lincoln</i><br>")}
+
 if (navigator.userAgent.includes("Mozilla")) {
     window.location.href = "https://www.mozilla.org";
 }
 
 else {window.location.href = "https://www.gigantti.fi"}
-
-for (let i = 0; i < 50; i++) {
-
-document.write('<strong><i>"If I had nine hours to chop down a tree, I\'d spend the first six sharpening my ax."</i></strong><br>')
-document.write("<i>-- Abraham Lincoln</i><br>")}
---
+++ Workshop 2/Script/sivutin.js
@@ -0,0 +1,3 @@
+var Student = confirm("Are you a student?")
+if (Student) {document.write("Welcome student!")}
+else {document.write("Sorry, this page is for students only!")}
