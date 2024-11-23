4b5038f3e5c4d157eefb8f1fb6c5fae8f74c23e3
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-23 02:18:41 +0200

    ö

---
+++ Workshop 3/Script/kolmas.js
@@ -0,0 +1,46 @@
+function myFunction() {
+    alert("You clicked me! "+ Date());
+}
+
+function showTable() {
+    document.write("<table border='1' cellpadding='10'>");
+            document.write("<tr>");
+            document.write("<th>Name</th>");
+            document.write("<th>Position</th>");
+            document.write("<th>Salary</th>");
+            document.write("</tr>");
+
+            document.write("<tr>");
+            document.write("<td>Tiger Nixon</td>");
+            document.write("<td>System Architect</td>");
+            document.write("<td>$320,800</td>");
+            document.write("</tr>");
+
+            document.write("<tr>");
+            document.write("<td>Garrett Winters</td>");
+            document.write("<td>Accountant</td>");
+            document.write("<td>$170,750</td>");
+            document.write("</tr>");
+
+            document.write("<tr>");
+            document.write("<td>Ashton Cox</td>");
+            document.write("<td>Junior Technical Author</td>");
+            document.write("<td>$86,000</td>");
+            document.write("</tr>");
+
+            document.write("<tr>");
+            document.write("<td>Cedric Kelly</td>");
+            document.write("<td>Senior Javascript Developer</td>");
+            document.write("<td>$433,060</td>");
+            document.write("</tr>");
+
+            document.write("<tr>");
+            document.write("<td>Airi Satou</td>");
+            document.write("<td>Accountant</td>");
+            document.write("<td>$162,700</td>");
+            document.write("</tr>");
+}
+
+function Geolocation() {
+    alert(GeolocationCoordinates)
+}
