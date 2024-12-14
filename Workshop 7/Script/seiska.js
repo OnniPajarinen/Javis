e13ed046474f496cb8f903e07ba8d8a79497d1cc
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-14 12:22:19 +0200

    d

---
+++ Workshop 7/Script/seiska.js
@@ -0,0 +1,8 @@
+parseData(){
+    document.getElementsByTagName(quotes)
+    for (let i=0; i < quotes.length; i++) {
+        const quote = quotes[i].getElementsByTagName("quote");
+        const author = quotes[i].getElementsByTagName("author");
+
+    }
+}
