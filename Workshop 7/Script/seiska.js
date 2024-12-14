5761633aacd488230398460c8f870c18b6a4fc66
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-14 12:44:48 +0200

    o

--- Workshop 7/Script/seiska.js
+++ Workshop 7/Script/seiska.js
@@ -1,8 +1,14 @@
-parseData(){
-    document.getElementsByTagName(quotes)
-    for (let i=0; i < quotes.length; i++) {
-        const quote = quotes[i].getElementsByTagName("quote");
-        const author = quotes[i].getElementsByTagName("author");
+function parseData() {
+    let output = ""
+    var quotes = document.getElementsByTagName("quotes");
 
+    for (let i = 0; i < quotes.length; i++) {
+        var quote = quotes[i].getElementsByTagName("quote")[0].textContent;
+        var author = quotes[i].getElementsByTagName("author")[0].textContent;
+
+        output += `${quote}<br>${author}<br><br>`;
     }
+
+    document.getElementById("output").innerHTML = output;
 }
+
