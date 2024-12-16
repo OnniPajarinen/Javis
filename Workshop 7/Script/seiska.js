b915849963c75b3d337dc29f1d09219b877d255a
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-16 03:40:59 +0200

    d

--- Workshop 7/Script/seiska.js
+++ Workshop 7/Script/seiska.js
@@ -15,7 +15,7 @@ function parseData() {
 function loadXMLFile(){
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.open("GET", "https://quotes.rest/qod", true);
+    xmlhttp.open("GET", "http://quotes.rest/qod.xml", true);
     xmlhttp.send();
 
     xmlhttp.onreadystatechange = function () {
@@ -30,5 +30,51 @@ function loadXMLFile(){
 }
 
 function loadAndParseXML() {
+    let quoteOutput = ""
+    let authorOutput = ""
+    var tokaquot = document.getElementsByTagName("tokaquot");
 
+    for (let i = 0; i < tokaquot.length; i++) {
+        var quote = tokaquot[i].getElementsByTagName("quote")[0].textContent;
+        var author = tokaquot[i].getElementsByTagName("author")[0].textContent;
+
+        quoteOutput += `<p>${quote}</p>`;
+        authorOutput += `<p>${author}</p>`;
+        
+        document.getElementById("quu").innerHTML = quoteOutput;
+        document.getElementById("aa").innerHTML = authorOutput;
+}
+document.getElementById("turha").innerHTML = ""
+
+}
+
+function loadAndParseNews(fiidi) {
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET", fiidi, true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let xmldoc = xmlhttp.responseXML;
+
+            let items = xmldoc.getElementsByTagName("item");
+
+            let output = "<ul>";
+
+            for (let i = 0; i < items.length; i++) {
+                let title = items[i].getElementsByTagName("title")[0].textContent;
+                let link = items[i].getElementsByTagName("link")[0].textContent;
+
+                output += `<li><a href="${link}" target="_blank">${title}</a></li>`;
+            }
+
+            output += "</ul>";
+
+            document.getElementById("newsfeed").innerHTML = output;
+        } else if (xmlhttp.readyState == 4) {
+            document.getElementById("newsfeed").innerHTML =
+                "<p>Ei toimi! Pääsy hylätty linkkeihin, mutta koodini löytyy!</p>";
+        }
+    };
 }
