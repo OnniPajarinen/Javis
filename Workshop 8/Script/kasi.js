7d66df51dc640ccac63a6924f1cfc76dbcf3bd18
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-17 03:58:23 +0200

    s

--- Workshop 8/Script/kasi.js
+++ Workshop 8/Script/kasi.js
@@ -1,7 +1,19 @@
-function eka(){
+var text = '{ "employees" : [' +
+			'{ "firstName":"John" , "lastName":"Doe" },' +
+			'{ "firstName":"Anna" , "lastName":"Smith" },' +
+			'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
-}
+
 
 function toka(){
-    
+    document.getElementById("omg").innerHTML = text
+}
+
+function eka(){
+    var joo = JSON.parse(text);
+console.log(joo.employees);
+
+joo.employees.forEach(function(employee) {
+    console.log("First Name: " + employee.firstName + ", Last Name: " + employee.lastName);
+});
 }
